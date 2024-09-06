dfe1c6508196a0fc05867329d0425caa52d732af
xuhuan lu <102048940+huanhuan0728@users.noreply.github.com>
2024-09-06 22:40:59 +0800

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/* 
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -6,4 +7,49 @@ console.log(23);
 
 let firstName = "Matila";
 console.log(firstName);
+
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+console.log(year);
+console.log(typeof year);
+
+
+
+let age = 36;
+age = 31;
+const birthday = 1991;
+
+var job = 'programmer';
+job = 'teacher';
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtman';
+console.log(firstName + '' + lastName);
+
+// comparison operators
+console.log(ageJonas > ageSarah);
+const isFullAge = ageJonas >= 18;
+*/
 
